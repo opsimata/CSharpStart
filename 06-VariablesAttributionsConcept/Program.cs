8e98c2d530628720273e67de7f44804978cf3bbf
Pedro <pedro.enrique1599@icloud.com>
2023-03-26 06:23:05 -0300

    C# variables attribuitions

---
+++ 06-VariablesAttributionsConcept/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Variables Attributions Concept:");
+
+        int age = 30;
+
+        int agePerson = age;
+
+        Console.WriteLine("\nPerson 1 age: " + agePerson);
+
+        age = 23;
+
+        //No hoisting on C#.
+
+        Console.Write("\nPress any key to close...");
+        Console.ReadLine();
+    }
+}
