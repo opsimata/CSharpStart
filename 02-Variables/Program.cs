a5d41d7859ed6c7e913a438e497dfbf323505b1c
Pedro <pedro.enrique1599@icloud.com>
2023-03-26 03:03:31 -0300

    variables

---
+++ 02-Variables/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Creating Variables");
+
+        int age;
+        age = 23;
+
+        Console.WriteLine("Age: " + age);
+
+
+        age = 34 * 2 + 1;
+        age = 4 * (2 + 3);
+
+        Console.WriteLine("Age: " + age);
+
+        Console.WriteLine("Press any key to close...");
+        Console.ReadLine();
+
+    }
+}
