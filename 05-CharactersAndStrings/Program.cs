d712ebff11edba6249d6f7858fd0f4951baa3047
Pedro <pedro.enrique1599@icloud.com>
2023-03-26 06:13:02 -0300

    char and strings

--- 05-CharactersAndStrings/Program.cs
+++ 05-CharactersAndStrings/Program.cs
@@ -15,6 +15,23 @@ class Program
         character = (char)(65 + 1);
         Console.WriteLine("\nCharacter: " + character);
 
+        string phrase = "Lorem";
+        Console.WriteLine("\nPhrase: " + phrase + ", " + 2023);
+
+        string emptyString = "";
+        Console.WriteLine("\nEmpty String: " + emptyString);
+
+        Console.WriteLine("\n");
+
+        string itemString = @"Exemple:
+    - Break 1
+    - Break 2
+    - Break 3"
+    ;
+        Console.WriteLine(itemString);
+
+        //char can't be empty.
+
         Console.Write("\nPress any key to close...");
         Console.ReadLine();
     }
