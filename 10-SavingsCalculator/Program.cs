d25f71729138bffcd450d7c16ae0e19a016fd0a5
Pedro <pedro.enrique1599@icloud.com>
2023-03-29 01:56:32 -0300

    While exercise example

---
+++ 10-SavingsCalculator/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.Write("Savings Calculator:\n");
+
+        double deposits = 1000;
+        int month = 1;
+
+        //monthly return: 0.5% + 1000 deposit.
+
+        while (month <= 12)
+        {
+            deposits = deposits + deposits * 0.005;
+            Console.Write("\nTotal income in month " + month + ": $" + deposits);
+            month++;
+        }
+
+
+
+        Console.Write("\n\nPress any key to close...");
+        Console.ReadLine();
+    }
+}
