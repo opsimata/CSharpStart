09cc208cc37f62d1b65d0d462964d4dcfc44a395
Pedro <pedro.enrique1599@icloud.com>
2023-03-28 23:48:18 -0300

    Conditional Statements

---
+++ 07-ConditionalStatements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Conditional Statements:");
+
+        int age = 16, qttPeople = 2;
+
+        if (age >= 18)
+        {
+            Console.WriteLine("\nAllowed in!");
+        }
+        else
+        {
+            if (qttPeople => 2)
+            {
+                Console.WriteLine("\nAllowed in with a legal of age companion!");
+            }
+            else
+            {
+                Console.WriteLine("\nNot allowed!");
+            }
+        }
+
+        Console.Write("\nPress any key to close...");
+        Console.ReadLine();
+    }
+}
