5e3245e83cfb715da0acb0a754155995b88696a1
Pedro <pedro.enrique1599@icloud.com>
2023-03-29 01:25:05 -0300

    code scope concepts

---
+++ 09-ProjectScope/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Conditional Statements:");
+
+        int age = 16, qttPeople = 2;
+
+        bool accompanied = qttPeople >= 2;
+
+        string addtText;
+
+        if (accompanied == true)
+        {
+            addtText = "The guest is accompanied!";
+        }
+        else
+        {
+            addtText = "The guest is underaged and isn't accompanied!";
+        }
+
+        if (age >= 18 || accompanied)
+        {
+            Console.WriteLine("\nAllowed in!");
+            Console.WriteLine("\n" + addtText);
+        }
+        else
+        {
+            Console.WriteLine("\nNot allowed!");
+            Console.WriteLine("\n" + addtText);
+        }
+
+        Console.Write("\nPress any key to close...");
+        Console.ReadLine();
+    }
+}
