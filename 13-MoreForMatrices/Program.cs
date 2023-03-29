f546f6dea703cb9575d2dc194d085516fa5ae50e
Pedro <pedro.enrique1599@icloud.com>
2023-03-29 03:55:09 -0300

    for matrix loop example

---
+++ 13-MoreForMatrices/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Long term savings calculator:\n\n");
+
+        double appreciationIndex = 1.005, investment = 1000;
+
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        for (int lines = 1; lines <= 5; lines++)
+        {
+            for (int columns = 1; columns <= 10; columns++)
+            {
+                Console.Write("*");
+                if (columns >= lines)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.Write("\nPress any key to close...");
+        Console.ReadLine();
+    }
+}
